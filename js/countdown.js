718391527495038e5ce0618b8ad4332dfe4565dd
ElofHew <2352235176@qq.com>
2024-12-14 19:21:02 +0800

    Add files via upload

--- js/countdown.js
+++ js/countdown.js
@@ -1,19 +1,21 @@
 // Countdown
 
-const countdownTarget = new Date("2024-12-14T00:00:00").getTime();
+const targetDate = new Date("December 25, 2024 00:00:00").getTime();
 
-const countdownFunction = setInterval(() => {
-    const now = new Date().getTime();
-    const distance = countdownTarget - now;
+function updateCountdown() {
+    const currentDate = new Date().getTime();
+    const distance = targetDate - currentDate;
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    document.getElementById("countdown-text").innerHTML = `距离我的生日还有：<br /><span style="color: tan;">${days}天 ${hours}小时 ${minutes}分钟 ${seconds}秒</span>`;
     
+    document.getElementById("countdown-text").innerHTML = `距离圣诞节还有：<br /><span style="color: tan;">${days}天 ${hours}小时 ${minutes}分钟 ${seconds}秒</span>`;
+
     if (distance < 0) {
-        clearInterval(countdownFunction);
-        document.getElementById("countdown-text").innerHTML = '<span>祝我<br /><b style="color: orangered;">15岁生日快乐</b></span>';
+        clearInterval(countdownInterval);
+        document.getElementById("countdown").innerHTML = `<b style="color: orangered;">圣诞节快乐！</b>`;
     }
-}, 1000);
+}
+
+const countdownInterval = setInterval(updateCountdown, 1000);
