6f8f48b4720fae3fab9d46fb2e9828e83d922f47
ElofHew <2352235176@qq.com>
2025-08-18 23:25:04 +0800

    Update donate.js

--- js/donate.js
+++ js/donate.js
@@ -6,11 +6,8 @@ document.getElementById("donate-list").innerHTML = `
 <caption><b>赞助列表</b></caption>
 <tbody>
 <tr><td><b>赞助人</b></td><td><b>时间</b></td><td><b>金额</b></td></tr>
-<tr><td>Hanson</td><td>25-01-19</td><td>￥4.18</td></tr>
 <tr><td>lu</td><td>25-02-01</td><td>￥3.09</td></tr>
 <tr><td>graphic</td><td>25-02-01</td><td>￥1.63</td></tr>
 <tr><td>lu</td><td>25-01至25-05</td><td>总共16个月QQ超级会员</td></tr>
-<tr><td>Hanson</td><td>25-03-23</td><td>三个月QQ空间黄钻</td></tr>
-<tr><td>Hanson</td><td>25-05-24</td><td>￥21.54</td></tr>
 </tbody>
 </table>`;
