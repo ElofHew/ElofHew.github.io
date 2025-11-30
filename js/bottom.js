e9d2ff7f4caba542552f7e56d1f4b3bf26727356
ElofHew <2352235176@qq.com>
2025-11-30 14:07:48 +0800

    Update 20251130

--- js/bottom.js
+++ js/bottom.js
@@ -1,10 +1,21 @@
 // Bottom Navigation Bar
 
-document.addEventListener("DOMContentLoaded", function() {
+document.getElementById("footer-prt").innerHTML = `
+<div class="footer" id="footer">
+    <a href="https://elofhew.github.io/"><img src="icons/main.svg" width="30" height="30" /></a>
+    <a href="https://elofhew.github.io/download.html"><img src="icons/dl.svg" width="30" height="30" /></a>
+    <a href="https://elofhew.github.io/Blogs/" target="_blank"><img src="icons/blog.svg" width="30" height="30" /></a>
+    <a href="https://elofhew.github.io/friendlink.html"><img src="icons/link.svg" width="30" height="30" /></a>
+    <a href="https://elofhew.github.io/music.html"><img src="icons/music.svg" width="30" height="30" /></a>
+    <a href="https://elofhew.github.io/game.html"><img src="icons/game.svg" width="30" height="30" /></a>
+</div>
+`
+
+document.addEventListener("DOMContentLoaded", function () {
     var lastScrollTop = 0;
     var footer = document.getElementById("footer");
 
-    window.addEventListener("scroll", function() {
+    window.addEventListener("scroll", function () {
         var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
         if (scrollTop > lastScrollTop) {
--- js/head.js
+++ js/head.js
@@ -1,25 +1,9 @@
 // top
 document.getElementById("head-top").innerHTML = `
-<p>是的孩子们，<a href="https://ek457.github.io/" target="_blank">OakStudio</a> 推出了超级强的Python伪系统：<a href="https://ek457.github.io/qos/" target="_blank">Quarter OS</a></p>
+<p>时间转瞬即逝，2025年马上就要过去了……</p>
 `;
 
 // warn
 document.getElementById("warn-top").innerHTML = `
-<p>通知：域名即将过期，请牢记永久域名：elofhew.github.io</p>
+<p>网站已更换域名为：www.danevan.top</p>
 `;
-
-// notice
-document.getElementById('notice').innerHTML = `
-<ul>
-    <li><details><summary><b>2025.07.28</b></summary>想起来收拾网站了，更换MiSans字体调用方式，使加载速度更快</details></li>
-    <li><details><summary><b>2025.04.18</b></summary>新增菏泽初中信息技术考试练习程序安装教程</details></li>
-    <li><details><summary><b>2025.03.29</b></summary>JavaScript整理</details></li>
-    <li><details><summary><b>2025.02.22</b></summary>大部分需要动态更新的信息内容已改用链接JavaScript方式展现，请务必开启JavaScript以正常浏览本站！</details></li>
-    <li><details><summary><b>2025.02.05</b></summary>捐赠页面重新设计</details></li>
-    <li><details><summary><b>2025.01.29</b></summary>春节快乐！</details></li>
-    <li><details><summary><b>2025.01.11</b></summary>修复部分显示错误</details></li>
-    <li><details><summary><b>2025.01.01</b></summary>新年快乐！</details></li>
-    <li><details><summary><b>2024.12.28</b></summary>新的起始页已上线<br />→<a href="https://komlers.github.io/" target="_blank">home.drevan.xyz</a></details></li>
-    <li><details><summary><b>2024.12.27</b></summary>本站域名由de233.us.kg更改为www.drevan.xyz（<a href="http://elofhew.github.io/Blogs/post/us.kg-mian-fei-yu-ming-ke-neng-bu-zai-wen-ding.html" target="_blank">为什么?</a>）</details></li>
-    <li><details><summary><b>2024.12.25</b></summary>孩子们，圣诞快乐</details></li>
-</ul>`;
